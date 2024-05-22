447d3daf2f4511cea11f0d250283bf740e7962e2
Thalita Miquiles <tatimiquiles@gmail.com>
2024-05-22 18:02:55 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,97 @@
+from tkinter import *
+from tkinter import ttk
+
+#cores
+cor1 = "#3b3831" #preto não tão forte
+cor2 = "#f5f4f2" #branco
+cor3 = "#272c45" #azul
+cor4 = "#dcdde3" #cinza
+cor5 = "#deb00b" #laranja
+
+
+janela =Tk()
+janela.title("Calculadora")
+janela.geometry("235x310")
+janela.config(bg=cor1)
+
+frame_tela = Frame(janela, width=235, height=50, bg=cor3)
+frame_tela.grid(row=0, column=0)
+
+frame_corpo = Frame(janela, width=235, height=268)
+frame_corpo.grid(row=1, column=0)
+
+# variável vazia
+todos_valores = ''
+
+# função
+def entrar_valores(event):
+
+    global todos_valores
+
+    todos_valores = todos_valores + str(event)
+
+    valor_texto.set(todos_valores)
+
+
+
+
+
+# label
+valor_texto = StringVar()
+
+app_label = Label(frame_tela, textvariable=valor_texto, width=16, height=2, padx=7, relief=FLAT, anchor="e", justify=RIGHT, font=('Ivy', 18), bg=cor3, fg=cor2)
+app_label.place(x=0, y=0)
+
+
+
+#botões
+# linha 1
+b_1 = Button(frame_corpo, text="C", width=11, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_1.place(x=0, y=0)
+b_2 = Button(frame_corpo, command = lambda:entrar_valores('%'), text="%", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_2.place(x=118, y=0)
+b_3 = Button(frame_corpo, text="/", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_3.place(x=177, y=0)
+
+# linha 2
+b_4 = Button(frame_corpo, text="7", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_4.place(x=0, y=52)
+b_5 = Button(frame_corpo, text="8", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_5.place(x=59, y=52)
+b_6 = Button(frame_corpo, text="9", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_6.place(x=118, y=52)
+b_7 = Button(frame_corpo, text="*", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_7.place(x=177, y=52)
+
+# linha 3
+b_8 = Button(frame_corpo, text="4", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_8.place(x=0, y=104)
+b_9 = Button(frame_corpo, text="5", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_9.place(x=59, y=104)
+b_10 = Button(frame_corpo, text="6", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_10.place(x=118, y=104)
+b_11 = Button(frame_corpo, text="-", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_11.place(x=177, y=104)
+
+# linha 4
+b_12 = Button(frame_corpo, text="1", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_12.place(x=0, y=156)
+b_13 = Button(frame_corpo, text="2", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_13.place(x=59, y=156)
+b_14 = Button(frame_corpo, text="3", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_14.place(x=118, y=156)
+b_15 = Button(frame_corpo, text="+", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_15.place(x=177, y=156)
+
+# linha 5
+b_16 = Button(frame_corpo, text="0", width=11, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_16.place(x=0, y=208)
+b_17 = Button(frame_corpo, text=".", width=5, height=2, bg=cor4, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_17.place(x=118, y=208)
+b_18 = Button(frame_corpo, text="=", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy', 13, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_18.place(x=177, y=208)
+
+
+
+
+janela.mainloop()
